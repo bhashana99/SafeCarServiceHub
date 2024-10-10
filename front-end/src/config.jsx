c30aca21de8b0ea624a9b2e3f234f6d5421a562b
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2024-10-10 18:55:23 +0530

    change config file type

--- front-end/src/config.jsx
+++ front-end/src/config.jsx
@@ -1,7 +1,7 @@
 export const config = {
-  
-  signInRedirectURL: "https://localhost:3000/sign-in",
-  signOutRedirectURL: "https://localhost:3000/sign-out",
+
+  signInRedirectURL: "https://localhost:5173/sign-in",
+  signOutRedirectURL: "https://localhost:5173/sign-out",
   clientID: import.meta.env.VITE_CLIENT_ID,
   baseUrl: import.meta.env.VITE_BASE_URL,
 
