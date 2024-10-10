5d7f23873e0dc6e26e7074b895ca1fdf31e2e9d8
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2024-10-10 19:46:21 +0530

    login page and home page route

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 export default function App() {
- 
   return (
-    <div>App
-      
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />}  />
+      </Routes>
+    </BrowserRouter>
+  );
 }
--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { AuthProvider } from "@asgardeo/auth-react";
 import App from './App.jsx'
-import ReactDom from 'react-dom/client'
-import { config } from "./config.js";
+import ReactDOM from 'react-dom/client'
+import { config } from "./config.jsx";
 
 
-ReactDom.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthProvider config={ config }>
     <App />
   </AuthProvider>,
