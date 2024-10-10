9e7070be8791183ed405567cfb2e315b11744557
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2024-10-10 16:47:31 +0530

    add configure to main.jsx

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import { AuthProvider } from "@asgardeo/auth-react";
 import App from './App.jsx'
-import './index.css'
+import ReactDom from 'react-dom/client'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const config = {
+  signInRedirectURL: "https://localhost:3000/sign-in",
+  signOutRedirectURL: "https://localhost:3000/sign-out",
+  clientID: import.meta.env.VITE_CLIENT_ID,
+  baseUrl: import.meta.env.VITE_BASE_URL,
+  
+  scope: [ "openid","profile","email" ]
+};
+
+
+ReactDom.createRoot(document.getElementById('root')).render(
+  <AuthProvider config={ config }>
     <App />
-  </StrictMode>,
+  </AuthProvider>,
 )
