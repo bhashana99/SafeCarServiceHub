87db8fd327fafe80e0fab4a84dae68683c740346
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2024-10-10 19:45:27 +0530

    Design Login Page

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
+import '../style/loginPage.css';
 
 export default function Login() {
   return (
-    <div className='text-red-400'>Login</div>
-  )
+    <div className="body__container">
+      <h1>Vehicle Service Reservation</h1>
+      <h3>Please Log in to continue...</h3>
+      <form>
+        <div className="login__container">
+          <input
+            type="submit"
+            value="Log in"
+            id="submit__btn"
+          />
+        </div>
+      </form>
+    </div>
+  );
 }
