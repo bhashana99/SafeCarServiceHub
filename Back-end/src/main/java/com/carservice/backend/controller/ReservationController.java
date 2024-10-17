d312957d0f50b8a668408150eee0bae318a8c0aa
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2024-10-17 19:02:51 +0530

    add cross origin

--- Back-end/src/main/java/com/carservice/backend/controller/ReservationController.java
+++ Back-end/src/main/java/com/carservice/backend/controller/ReservationController.java
@@ -2,11 +2,11 @@ package com.carservice.backend.controller;
 
 import com.carservice.backend.model.ReservationModel;
 import com.carservice.backend.service.ReservationService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/reservations")
 public class ReservationController {
@@ -14,8 +14,10 @@ public class ReservationController {
     @Autowired
     private ReservationService reservationService;
 
+    @PostMapping("/create-reservation")
     public ReservationModel createReservation(@RequestBody ReservationModel reservationModel){
         return  reservationService.createReservation(reservationModel);
     }
+
 
 }
