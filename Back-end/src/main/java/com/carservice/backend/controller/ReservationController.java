01799cd131b15c4f1b5dcfb698c5c62487b72c90
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2024-10-17 16:53:50 +0530

    create createReservation method in reservationController  class

--- Back-end/src/main/java/com/carservice/backend/controller/ReservationController.java
+++ Back-end/src/main/java/com/carservice/backend/controller/ReservationController.java
@@ -1,10 +1,21 @@
 package com.carservice.backend.controller;
 
+import com.carservice.backend.model.ReservationModel;
+import com.carservice.backend.service.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/reservations")
 public class ReservationController {
+
+    @Autowired
+    private ReservationService reservationService;
+
+    public ReservationModel createReservation(@RequestBody ReservationModel reservationModel){
+        return  reservationService.createReservation(reservationModel);
+    }
 
 }
