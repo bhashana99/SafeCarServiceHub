4aa46f2217ca34ffc57ae1772f55d95ab2202c13
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2024-10-17 15:55:37 +0530

    create reservation repo interface

---
+++ Back-end/src/main/java/com/carservice/backend/repository/ReservationRepo.java
@@ -0,0 +1,13 @@
+package com.carservice.backend.repository;
+
+import com.carservice.backend.model.ReservationModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReservationRepo extends JpaRepository<ReservationModel,Long> {
+ List<ReservationModel> findByUsername(String username);
+ 
+}
