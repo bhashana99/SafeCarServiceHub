b4d2a367b02ccab5132907afb0d9df7a917f244b
Vidura12 <nayanodyav@gmail.com>
2024-10-19 18:53:09 +0530

    protected routes in app

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -4,15 +4,37 @@ import Login from "./pages/Login";
 import Home from "./pages/Home";
 import ReservationForm from "./pages/reservationForm";
 import ReservationDetails from "./pages/ReservationDetails";
+import { ProtectedRoute } from "./components/ProtectedRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/create-reservation" element={<ReservationForm />} />
-        <Route path="/reservation-details" element={<ReservationDetails />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/create-reservation"
+          element={
+            <ProtectedRoute>
+              <ReservationForm />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/reservation-details"
+          element={
+            <ProtectedRoute>
+              <ReservationDetails />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
